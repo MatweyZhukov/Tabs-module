90a5395121afc049ab1ffff6a59f2fe4895a51b1
Zhukov Matwey <zhukov.matwei@gmail.com>
2022-09-17 12:20:00 +0300

    tabs

---
+++ tabs.js
@@ -0,0 +1,40 @@
+function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
+	const tabs = document.querySelectorAll(tabsSelector),
+		tabsContent = document.querySelectorAll(tabsContentSelector),
+		tabsParent = document.querySelector(tabsParentSelector);
+
+	function hideTabContent() {
+		tabsContent.forEach(item => {
+			item.classList.remove('fade', 'show');
+			item.classList.add('hide');
+		});
+
+		tabs.forEach(item => {
+			item.classList.remove(activeClass);
+		});
+	}
+
+	function showTabContent(i = 0) {
+		tabsContent[i].classList.remove('hide');
+		tabsContent[i].classList.add('show', 'fade');
+		tabs[i].classList.add(activeClass);
+	}
+
+	hideTabContent();
+	showTabContent();
+
+	tabsParent.addEventListener('click', (e) => {
+		const target = e.target;
+
+		if (target && target.classList.contains(tabsSelector.slice(1))) {
+			tabs.forEach((item, i) => {
+				if (target == item) {
+					hideTabContent();
+					showTabContent(i);
+				}
+			});
+		}
+	});
+}
+
+export default tabs;
